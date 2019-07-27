ac99d2db4056c505d1ad6e10a6934bf0f3b3584d
FredGruetzner <fgruetzn@msudenver.edu>
2019-07-26 20:53:48 -0600

    adjust html layout,added nav bar and java function to contact links

---
+++ withoutFrameWork/jsfile.js
@@ -0,0 +1,13 @@
+function email() {
+  window.open('mailto:fred.gruetzner@gmail.com');
+}
+
+function gitHub() {
+  //return window.location.href = "https://github.com/FredGruetzner?tab=repositories";
+  window.open("https://github.com/FredGruetzner?tab=repositories");
+}
+
+function linkedIn() {
+  window.open("https://www.linkedin.com/in/frederick-gruetzner");
+  //return window.open("https://www.linkedin.com/in/frederick-gruetzner");
+}
