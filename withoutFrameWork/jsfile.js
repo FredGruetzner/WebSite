7ac6e01d148225f7cf6de728499d7e8e253e5dc0
FredGruetzner <fgruetzn@msudenver.edu>
2019-08-19 14:32:21 -0600

    added js fuctions

--- withoutFrameWork/jsfile.js
+++ withoutFrameWork/jsfile.js
@@ -1,3 +1,4 @@
+
 function email() {
   window.open('mailto:fred.gruetzner@gmail.com');
 }
@@ -10,4 +11,16 @@ function gitHub() {
 function linkedIn() {
   window.open("https://www.linkedin.com/in/frederick-gruetzner");
   //return window.open("https://www.linkedin.com/in/frederick-gruetzner");
+}
+
+function showDate() {
+	var time = new Date();
+	document.getElementById("date").innerHTML = time;
+}
+
+function passWord() {
+	var pass = new Array ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", 
+				"K", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
+	var select = Math.floor(Math.random()* 52);
+	document.getElementById("pword").innerHTML = pass[select];
 }
